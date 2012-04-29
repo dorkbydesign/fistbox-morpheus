f887ebc34c1fe3a53653ad719433c9713e210210
dorkbydesign <barton@natariangraphics.com>
2012-04-29 05:32:57 -0700

    OpenWRT Clean Slate

---
+++ tmp/.host.mk
@@ -0,0 +1,8 @@
+HOST_OS:=Linux
+HOST_ARCH:=i686
+GNU_HOST_NAME:=i386-redhat-linux
+TAR:=/bin/gtar
+FIND:=/fistbox-morpheus/OpenWrt-ImageBuilder-ar71xx-for-Linux-i686/staging_dir/host/bin/find
+BASH:=/bin/bash
+FIND_L=/fistbox-morpheus/OpenWrt-ImageBuilder-ar71xx-for-Linux-i686/staging_dir/host/bin/find -L $(1)
+XARGS:=xargs -r
